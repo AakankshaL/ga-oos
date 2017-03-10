337756a057e9c3e19c0b9d2f0812f80d39312bd5
shaaan <shantanulondhe26@gmail.com>
2017-03-10 17:06:04 +0530

    MDL, Auth, Database
    * MDL looks good. Lets use it
    * Done work with Firebase Auth, It works flawlessly
    * Retreive objects from the database - partially working

---
+++ public/appstuff.js
@@ -0,0 +1,65 @@
+// FirebaseUI config.
+var uiConfig = {
+    signInSuccessUrl: 'main.html'
+    , signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GithubAuthProvider.PROVIDER_ID
+          , firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ], // Terms of service url.
+    tosUrl: '<your-tos-url>'
+};
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+//Trace Auth over entire app
+initApp = function () {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            user.getToken().then(function (accessToken) {
+                document.getElementById('sign-in-status').textContent = 'Signed in';
+                document.getElementById('sign-in').textContent = 'Sign out';
+                document.getElementById('account-details').textContent = JSON.stringify({
+                    displayName: displayName
+                    , email: email
+                    , emailVerified: emailVerified
+                    , photoURL: photoURL
+                    , uid: uid
+                    , accessToken: accessToken
+                    , providerData: providerData
+                }, null, '  ');
+            });
+        }
+        else {
+            // User is signed out.
+            document.getElementById('sign-in-status').textContent = 'Signed out';
+            document.getElementById('sign-in').textContent = 'Sign in';
+            document.getElementById('account-details').textContent = 'null';
+        }
+    }, function (error) {
+        console.log(error);
+    });
+};
+logout = function () {
+        firebase.auth().signOut().then(function () {
+            //successful
+            window.location.href = "index.html";
+        });
+    }
+    // This works. Now just need to put the data in respective fields
+var database = firebase.database();
+/*var uid = firebase.auth().getUserId;*/
+var updateOrder = firebase.database().ref('salesman/');
+updateOrder.on('value', function (snapshot) {
+    console.log(snapshot.val());
+    document.getElementById('view-some').textContent = 'Order' + snapshot.val();
+}, function (error) {
+    console.log("Error: " + error.code);
+});
---
+++ public/config.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyCL0LOKi5cuhkFShdyA0ovnAiRO4kTbyZQ",
+    authDomain: "ga-onlineorders.firebaseapp.com",
+    databaseURL: "https://ga-onlineorders.firebaseio.com",
+    storageBucket: "ga-onlineorders.appspot.com",
+    messagingSenderId: "353809225290"
+  };
+  firebase.initializeApp(config);
